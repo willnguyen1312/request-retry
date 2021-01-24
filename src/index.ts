aa2fe97ce40e171fb6edee043e8a3a253a90c7a4
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-01-24 13:50:25 +0700

    Awesome

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+export interface RetryRequestOptions {
+  numOfAttempts?: number;
+  shouldRetry?: (context: { currentAttemp: number; error: any }) => boolean;
+}
+
+export type RetryRequest<T> = (
+  request: () => Promise<T>,
+  options?: RetryRequestOptions
+) => Promise<T>;
+
+export async function retryRequest<T>(
+  request: () => Promise<T>,
+  options: RetryRequestOptions = {}
+): Promise<T> {
+  const { numOfAttempts = 3, shouldRetry = () => true } = options;
+
+  let currentAttemp = 1;
+  let continueRetry = true;
+
+  while (continueRetry && currentAttemp < numOfAttempts) {
+    try {
+      const result = await request();
+      return result;
+    } catch (error) {
+      currentAttemp += 1;
+      continueRetry = shouldRetry({
+        currentAttemp,
+        error,
+      });
+    }
   }
-  return a + b;
-};
+  return request();
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,24 @@
-import { sum } from '../src';
+import { retryRequest } from '../src';
 
-describe('blah', () => {
-  it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
+const asyncMock = jest
+  .fn()
+  .mockRejectedValueOnce('first failed')
+  .mockRejectedValueOnce('second failed')
+  .mockResolvedValueOnce('second call');
+
+describe('retryRequest', () => {
+  it('should work', async () => {
+    try {
+      const result = await retryRequest<string>(() => asyncMock(), {
+        numOfAttempts: 3,
+        shouldRetry: (...args) => {
+          console.log(args);
+          return true;
+        },
+      });
+      expect(result).toBe('second call');
+    } catch (error) {
+      expect(error).toBe('second failed');
+    }
   });
 });
